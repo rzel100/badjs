3e38a83f630cc6b642f47914700e15e1a9a91d4c
RzeL <62789747+rzel100@users.noreply.github.com>
2021-08-16 23:10:09 +0700

    Create badfunc.js

---
+++ js/badfunc.js
@@ -0,0 +1,12 @@
+function thisIsBadBruh() {
+  var theFrame = 0;
+  var badBruh = setInterval(function(){
+    if(theFrame >= 6571){
+      clearInterval(badBruh);
+      document.getElementById("badapple").innerHTML = "Finished !. Thanks For Watching... RzeL... :3";
+    } else {
+      document.getElementById("badapple").innerHTML = badAppleComment[theFrame];
+    }
+    theFrame += 1;
+  }, 33);
+}
